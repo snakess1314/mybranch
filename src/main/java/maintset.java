9d2abd976beee22fc1501b499cef42e5eee18167
snake1314 <986529221@qq.com>
2021-08-12 16:28:31 +0800

    新加内容1

--- src/main/java/maintset.java
+++ src/main/java/maintset.java
@@ -1,5 +1,7 @@
 public class maintset {
     public static void main(String[] args) {
-        System.out.println("测试内容一");
+        System.out.println("测试内容二");
+        System.out.println("测试内容二");
+        System.out.println("测试内容二");
     }
 }
