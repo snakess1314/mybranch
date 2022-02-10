645b66101b07064548306f1bde2fbca6f587fbf5
吴照强 <986529221@qq.com>
2022-02-10 09:08:00 +0800

    新加内容13

--- src/main/java/maintset.java
+++ src/main/java/maintset.java
@@ -2,6 +2,6 @@ public class maintset {
     public static void main(String[] args) {
         System.out.println("测试内容二");
         System.out.println("测试内容二");
-        System.out.println("测试内容二");
+        System.out.println("测试内容master");
     }
 }
