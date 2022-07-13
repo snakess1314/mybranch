f3d2c13caa6a65584508194b1e521ff10390ef6d
吴照强 <986529221@qq.com>
2022-07-13 10:02:37 +0800

    开发完成

---
+++ src/main/java/com/wzq/maintset.java
@@ -0,0 +1,7 @@
+package com.wzq;
+
+public class maintset {
+    public static void main(String[] args) {
+        System.out.println("第一版开发完成");
+    }
+}
--- src/main/java/maintset.java
+++
@@ -1,7 +0,0 @@
-public class maintset {
-    public static void main(String[] args) {
-        System.out.println("测试内容二");
-        System.out.println("测试内容二");
-        System.out.println("测试内容master");
-    }
-}
