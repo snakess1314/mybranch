c5a9eb31b3c942aaf33f44aa3728a30f496229ab
吴照强 <986529221@qq.com>
2022-07-13 10:14:13 +0800

    二次修复新加文件

---
+++ src/main/java/com/wzq/Test2.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2022
+ * 鲲云（北京）科技有限公司
+ */
+package com.wzq;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("二次修复得整改文件");
+    }
+}
