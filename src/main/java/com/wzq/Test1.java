8e0250ff131613ccac6886b554337f0fe444aecc
吴照强 <986529221@qq.com>
2022-07-13 10:13:22 +0800

    二次修复

--- src/main/java/com/wzq/Test1.java
+++ src/main/java/com/wzq/Test1.java
@@ -8,5 +8,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("开发第一期内容1.4");
         System.out.println("一期bug修复v1.41");
+        System.out.println("一期bug二次修复v1.41");
     }
 }
